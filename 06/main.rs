99626efff726211cb5142f7366b53dc53002fecf
Nikon The Third <nikon@nurfuerspam.de>
2022-12-07 22:56:19 +0100

    06_3

--- 06/main.rs
+++ 06/main.rs
@@ -1,17 +1,14 @@
 use std::{collections::HashSet, error::Error, fs::read_to_string};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let input = read_to_string("input.txt")?;
+    let input = read_to_string("input.txt")?.chars().collect::<Vec<_>>();
 
-    for size in [4, 14] {
+    Ok(for size in [4, 14] {
         let (index, _) = input
-            .chars()
-            .collect::<Vec<_>>()
             .windows(size)
             .enumerate()
             .find(|(_, chars)| chars.iter().collect::<HashSet<_>>().len() == size)
             .ok_or("no match")?;
         println!("2022-12-06 Part {}: {}", size / 10 + 1, index + size);
-    }
-    Ok(())
+    })
 }
